5b1f902d3bc40d1a7abae5de3614d2347d95f5aa
LLevente <ladanyi.levente@diak.szbi-pg.hu>
2025-11-27 14:14:29 +0100

    Update videoteka.sql

---
+++ videoteka.sql
@@ -0,0 +1,10 @@
+--2.feladat
+DROP DATABASE videotekaadatbazis IF EXISTS;
+CREATE DATABASE videotekaadatbazis IF NOT EXISTS
+  CHARACTER SET utf8mb4
+  COLLATE utf8mb4_hungarian_ci;
+
+--3.feladat
+USE videotekaadatbazis;
+
+--4.feladat
