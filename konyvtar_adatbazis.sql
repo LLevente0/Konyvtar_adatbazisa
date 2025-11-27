7d62e66d4ccc63ac2eeea3bea9414f3d1ee4536c
LLevente <ladanyi.levente@diak.szbi-pg.hu>
2025-11-27 14:02:23 +0100

    d

--- konyvtar_adatbazis.sql
+++ konyvtar_adatbazis.sql
@@ -1,12 +1,14 @@
-#2.feladat
-CREATE DATABASE konyvtaradatbazis;
+-- 2.feladat
+DROP DATABASE IF EXISTS konyvtaradatbazis;
+
+CREATE DATABASE konyvtaradatbazis
     CHARACTER SET utf8mb4
     COLLATE utf8mb4_hungarian_ci;
 
-#3.feladat
+-- 3.feladat
 USE konyvtaradatbazis;
 
-#4.feladat
+-- 4.feladat
 DROP TABLE IF EXISTS konyvek;
 CREATE TABLE IF NOT EXISTS konyvek (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
@@ -15,11 +17,12 @@ CREATE TABLE IF NOT EXISTS konyvek (
     kiadasi_ev YEAR,
     oldalszam INTEGER CHECK (oldalszam BETWEEN 20 AND 2000),
     kategoria ENUM ("fantasy", "sci-fi", "disztópia", "történelem"),
-    ertekeles FLOAT DEFAULT (5.0));
-)
+    ertekeles FLOAT DEFAULT (5.0)
+);
 
-#5.feladat
-CREATE TABLE olvasok (
+-- 5.feladat
+DROP TABLE IF EXISTS olvasok;
+CREATE TABLE IF NOT EXISTS olvasok (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nev VARCHAR(100) NOT NULL,
     szuletesi_datum DATE NOT NULL,
